4174b38a3a8323c00c6bdcc248d323a8060f8036
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-22 22:41:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,9 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 && 5 == 0
-    "Fizzbuzz"
     "Fizz"
-  #elsif int % 5 == 0
-    # "Buzz"
+  elsif int % 5 == 0
+     "Buzz"
  #elsif int % 3 == 0
  
     #"FizzBuzz"
